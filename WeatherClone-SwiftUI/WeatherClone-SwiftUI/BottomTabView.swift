0d8cb2b2fee784e622d0f16bb60893aa5c8cefd9
binisnil <dbsqlssss0704@gmail.com>
2024-10-07 14:42:50 +0900

    [Week1] 1주차 과제 및 레포지토리 세팅

---
+++ WeatherClone-SwiftUI/WeatherClone-SwiftUI/BottomTabView.swift
@@ -0,0 +1,72 @@
+//
+//  BottomTabView.swift
+//  WeatherClone-SwiftUI
+//
+//  Created by 박윤빈 on 9/23/24.
+//
+
+import SwiftUI
+
+enum Tab {
+    case map
+    case list
+    case main
+}
+
+struct BottomTabView: View {
+    @Binding var selectedTab: Tab
+    var body: some View {
+        VStack {
+            Divider()
+                .frame(minHeight: 0.5)
+                .overlay(.gray)
+            HStack {
+                Button {
+                    print("map button tapped")
+                } label: {
+                    Image(systemName: "map")
+                        .resizable()
+                        .foregroundStyle(.white)
+                        .frame(width: 27, height: 25)
+
+                }
+                .padding(.leading, 20)
+                
+                Spacer()
+                
+                HStack {
+                    Image(systemName: "location.fill")
+                        .resizable()
+                        .foregroundStyle(.white)
+                        .frame(width: 14, height: 13)
+                    
+                    Image(systemName: "circle.fill")
+                        .resizable()
+                        .foregroundStyle(.white)
+                        .frame(width: 9, height: 9)
+                }
+                Spacer()
+
+                
+                Button {
+                    print("list button tapped")
+                } label: {
+                    Image(systemName: "list.bullet")
+                        .resizable()
+                        .foregroundColor(.white)
+                        .frame(width: 26, height: 25)
+
+                }
+                .padding(.trailing, 20)
+
+            }
+            .background(Color.clear)
+            .frame(width: UIScreen.main.bounds.width, height: 78)
+        }
+    }
+}
+
+
+#Preview {
+    MainView()
+}
---
+++ WeatherClone-SwiftUI/WeatherClone-SwiftUI/WeatherClone_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherClone_SwiftUIApp.swift
+//  WeatherClone-SwiftUI
+//
+//  Created by 박윤빈 on 9/23/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherClone_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
