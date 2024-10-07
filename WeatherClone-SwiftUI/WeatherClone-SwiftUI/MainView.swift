e320081fe758d449066c44613c4db48d75c36985
binisnull <dbsqlssss0704@gmail.com>
2024-10-07 20:53:29 +0900

    [Week3] #5 - 상단 날씨 요약 뷰 분리

--- WeatherClone-SwiftUI/WeatherClone-SwiftUI/MainView.swift
+++ WeatherClone-SwiftUI/WeatherClone-SwiftUI/MainView.swift
@@ -17,15 +17,6 @@ struct MainView: View {
                 BottomTabView(selectedTab: $tabStatus)
                     .background(Color.clear)
                     .offset(x: 0, y: (UIScreen.main.bounds.height / 2) - 60)
-                
-                WeatherTimelineView()
-                    .background(Color.clear)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 15)
-                            .stroke(.gray.opacity(0.25), lineWidth: 0.5)
-                    )
-                    .frame(height: 212)
-                    .padding(.horizontal, 20)
         }
     }
 }
